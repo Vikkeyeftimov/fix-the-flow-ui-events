33d998a13e03c925c916806536dd5a2441f58143
Vikkeyeftimov <Vikksaus@hotmail.com>
2023-12-12 13:56:37 +0100

    nieuw
    nieuwew =

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,14 +7,17 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
+//1 start click
 let fronted = document.querySelector('a:nth-of-type(1)')
 
 fronted.addEventListener('dblclick', front)
 function front() {
   fronted.classList.toggle('fronted')
 }
+ //1 end 
 
 
+ //2 start keydown
 let design = document.querySelector('a:nth-of-type(2)')
 
 design.addEventListener('keypress', knopje)
@@ -22,3 +25,19 @@ function knopje(e) {
   if(e.key == 'o') {
     design.classList.toggle('design');
   } }
+  //2 end 
+
+
+  //3 start keydown
+  let en = document.querySelector('a:nth-of-type(3)')
+
+ en.addEventListener('keydown', knoppen)
+   function knoppen(e) {
+  if(e.key == 's') {
+    en.classList.toggle('en') ;
+  } }
+
+//3 end
+
+
+//4 
